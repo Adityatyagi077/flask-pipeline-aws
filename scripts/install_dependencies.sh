b4641c339296475a4470d36c5754027ab2c0ec85
Aditya Tyagi <adityatyagi3149@gmail.com>
2026-09-20 14:59:44 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,10 +4,18 @@ set -e
 
 APP_DIR="/var/www/flask-cicd"
 
+echo "Installing dependencies..."
+
+# Create application directory
 mkdir -p $APP_DIR
 
+# Create virtual environment
 python3 -m venv $APP_DIR/venv
 
-$APP_DIR/venv/bin/pip install --upgrade pip
+# Upgrade pip
+$APP_DIR/venv/bin/python -m pip install --upgrade pip
 
+# Install application dependencies
 $APP_DIR/venv/bin/pip install -r $APP_DIR/requirements.txt
+
+echo "Dependencies installed successfully."
